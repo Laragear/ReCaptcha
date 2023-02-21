d56989bf44461ce7c2e928965f5f70a9e24cc71c
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2023-02-20 23:00:50 -0300

    Initial PHP 8.2, Laravel 10 support

--- tests/Http/Middleware/UsesRoutesWithMiddleware.php
+++ tests/Http/Middleware/UsesRoutesWithMiddleware.php
@@ -2,33 +2,34 @@
 
 namespace Tests\Http\Middleware;
 
-use function app;
+use Closure;
+use Illuminate\Contracts\Foundation\Application;
 use Laragear\ReCaptcha\Http\ReCaptchaResponse;
 
 trait UsesRoutesWithMiddleware
 {
-    protected function defineEnvironment($app)
+    protected function defineEnvironment($app): void
     {
         $app->make('config')->set('recaptcha.enable', true);
     }
 
-    protected function defineRoutes($router)
+    protected function defineRoutes($router): void
     {
-        $router->post('v3/default', [__CLASS__, 'returnSameResponse'])->middleware('recaptcha.score');
-        $router->post('v3/threshold_1', [__CLASS__, 'returnSameResponse'])->middleware('recaptcha.score:1.0');
-        $router->post('v3/threshold_0', [__CLASS__, 'returnSameResponse'])->middleware('recaptcha.score:0');
-        $router->post('v3/action_foo', [__CLASS__, 'returnSameResponse'])->middleware('recaptcha.score:null,foo');
-        $router->post('v3/input_bar', [__CLASS__, 'returnSameResponse'])->middleware('recaptcha.score:null,null,bar');
+        $router->post('v3/default', Closure::fromCallable([static::class, 'returnSameResponse']))->middleware('recaptcha.score');
+        $router->post('v3/threshold_1', Closure::fromCallable([static::class, 'returnSameResponse']))->middleware('recaptcha.score:1.0');
+        $router->post('v3/threshold_0', Closure::fromCallable([static::class, 'returnSameResponse']))->middleware('recaptcha.score:0');
+        $router->post('v3/action_foo', Closure::fromCallable([static::class, 'returnSameResponse']))->middleware('recaptcha.score:null,foo');
+        $router->post('v3/input_bar', Closure::fromCallable([static::class, 'returnSameResponse']))->middleware('recaptcha.score:null,null,bar');
 
-        $router->post('v2/checkbox', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:checkbox');
-        $router->post('v2/checkbox/input_bar', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:checkbox,null,bar');
-        $router->post('v2/checkbox/remember', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:checkbox,10');
-        $router->post('v2/invisible', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:invisible');
-        $router->post('v2/invisible/input_bar', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:invisible,null,bar');
-        $router->post('v2/invisible/remember', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:invisible,10');
-        $router->post('v2/android', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:android');
-        $router->post('v2/android/input_bar', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:android,null,bar');
-        $router->post('v2/android/remember', [__CLASS__, 'returnResponseIfExists'])->middleware('recaptcha:android,10');
+        $router->post('v2/checkbox', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:checkbox');
+        $router->post('v2/checkbox/input_bar', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:checkbox,null,bar');
+        $router->post('v2/checkbox/remember', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:checkbox,10');
+        $router->post('v2/invisible', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:invisible');
+        $router->post('v2/invisible/input_bar', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:invisible,null,bar');
+        $router->post('v2/invisible/remember', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:invisible,10');
+        $router->post('v2/android', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:android');
+        $router->post('v2/android/input_bar', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:android,null,bar');
+        $router->post('v2/android/remember', Closure::fromCallable([static::class, 'returnResponseIfExists']))->middleware('recaptcha:android,10');
     }
 
     public static function returnSameResponse(ReCaptchaResponse $response): ReCaptchaResponse
@@ -36,8 +37,8 @@ trait UsesRoutesWithMiddleware
         return $response;
     }
 
-    public static function returnResponseIfExists(): ?ReCaptchaResponse
+    public static function returnResponseIfExists(Application $app): ?ReCaptchaResponse
     {
-        return app()->has(ReCaptchaResponse::class) ? app(ReCaptchaResponse::class) : null;
+        return $app->has(ReCaptchaResponse::class) ? $app->make(ReCaptchaResponse::class) : null;
     }
 }
